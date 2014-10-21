6572c42481bac10bbad014e15621fa872cec75e9
Emmanuel Asante <emmanuela.asante@gmail.com>
2014-10-21 06:14:19 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Author.create!(first_name: 'Agatha', last_name: 'Christie')
+Author.create!(first_name: 'James', last_name: 'Chase')
+Author.create!(first_name: 'Sidney', last_name: 'Sheldon')
+Author.create!(first_name: 'Obie', last_name: 'Fernandez')
+Author.create!(first_name: 'Cloves', last_name: 'Carneiro')
+Author.create!(first_name: 'Rida', last_name: 'Al Barazi')
+
+Publisher.create!( [ { name: 'Wiley' }, { name: 'Addison Wesley' },
+                     { name: 'Peachpit Press' }, { name: 'Manning Publications' },
+                     { name: 'No Starch Press' }, { name: 'Apress' }
+])
+
+Book.create!(title: 'Gnuplot in Action', isbn: '978032177297-8', page_count: 518, price: 38.56, description: 'A very cool graph creating software', 
+             published_at: '2012-01-01', publisher_id: 3, book_cover: File.open(File.join(Rails.root, 'app/assets/images/gnuplot.jpg')))
+
